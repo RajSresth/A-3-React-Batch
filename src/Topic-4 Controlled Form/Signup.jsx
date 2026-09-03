e13798f499b76a2b4d2273c466d904a669d7a194
Shresth Rajput <rajshresth2@gmail.com>
2026-09-03 15:27:30 +0530

    Controlled Form End

--- src/Topic-4 Controlled Form/Signup.jsx
+++ src/Topic-4 Controlled Form/Signup.jsx
@@ -1,10 +1,28 @@
-import React from "react";
+import { useState } from "react";
 import "./signup.css";
+import { FaRegEye, FaRegEyeSlash } from "react-icons/fa";
 
 const Signup = () => {
+  const [fullname, setFullname] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleSignupForm = (e) => {
+    e.preventDefault();
+
+    console.log("fullname:", fullname);
+    console.log("email:", email);
+    console.log("password:", password);
+
+    setFullname("");
+    setEmail("");
+    setPassword("");
+  };
+
   return (
     <div className="container">
-      <form action="">
+      <form action="" onSubmit={handleSignupForm}>
         <div className="form__top ">
           <h2>Signup</h2>
           <p>It will take only 3 sec.</p>
@@ -18,6 +36,8 @@ const Signup = () => {
               type="text"
               id="fullname_input"
               placeholder="Enter fullname"
+              value={fullname}
+              onChange={(e) => setFullname(e.target.value)}
               required
             />
           </div>
@@ -30,6 +50,8 @@ const Signup = () => {
               type="email"
               id="email_input"
               placeholder="xyz@gmail.com"
+              value={email}
+              onChange={(e) => setEmail(e.target.value)}
               required
             />
           </div>
@@ -38,12 +60,27 @@ const Signup = () => {
             <label htmlFor="password_input">
               Password<sup>*</sup>
             </label>
-            <input
-              type="password"
-              id="password_input"
-              placeholder="at least 8 characters"
-              required
-            />
+
+            <div className="password_input_container">
+              <input
+                type={showPassword ? "text" : "password"}
+                id="password_input"
+                placeholder="at least 8 characters"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+              <div
+                className="icon_container"
+                onClick={() => setShowPassword(!showPassword)}
+              >
+                {showPassword ? (
+                  <FaRegEye size={22} />
+                ) : (
+                  <FaRegEyeSlash size={22} />
+                )}
+              </div>
+            </div>
           </div>
         </div>
         <div className="form__bottom">
